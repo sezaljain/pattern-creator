dbce2038abd48cf6ca94ff7e7a5bad754b7d4fa3
Sezal Jain <sezaljain90@gmail.com>
2025-01-31 11:41:23 +0530

    plot measurements

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 # Read the CSV files
 profile = pd.read_csv("profile_outline.csv")
 front = pd.read_csv("frontLaterMeasurements.csv")
+front_outline = pd.read_csv("front_outline.csv")
 
 # Create the plot
 plt.figure(figsize=(10, 8))
@@ -12,7 +13,12 @@ plt.figure(figsize=(10, 8))
 plt.plot(profile["X"], profile["Y"], "b-", label="Profile Outline")
 
 # Plot front measurements
-plt.scatter(front["X"], front["Y"], color="red", label="Front Measurements")
+plt.scatter(front["X1"], front["Y"], color="red", label="Front Measurements")
+plt.scatter(front["X2"], front["Y"], color="red", label="Front Measurements")
+
+# Plot front outline
+plt.plot(front_outline["X1"], front_outline["Y"], "g-", label="Front Outline Left")
+plt.plot(front_outline["X2"], front_outline["Y"], "g-", label="Front Outline Right")
 
 plt.grid(True)
 plt.xlabel("X")
@@ -21,4 +27,4 @@ plt.title("Profile and Front Measurements")
 plt.legend()
 
 plt.savefig("plot.png")
-plt.close()
+plt.show()
